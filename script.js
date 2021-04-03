25656278cad32db936406dc37754ea10d3e2f7f2
Jason Leong <vergil_dmc1@hotmail.com>
2021-04-03 10:23:49 +1000

    Simple Validation
    Depending on validation display success or error styling

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// show input error functions
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  // overwriting the classname
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  //manipulating the DOM to grabb the parent element which has color border property
+  const formControl = input.parentElement;
+  /* overwriting the classname to our css class .form-control success 
+  to show that css code to get green border */
+  formControl.className = 'form-control success';
+}
+
+// Event Listeners
+form.addEventListener('submit', function (e) {
+  // prevent the form from submitting so we can check validation as no backend to submit to
+  e.preventDefault();
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else {
+    showSuccess(email);
+  }
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+  if (password2.value === '') {
+    showError(password2, 'Password is required');
+  } else {
+    showSuccess(password2);
+  }
+});
